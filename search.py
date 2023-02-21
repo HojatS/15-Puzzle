c63610df6ef7ca1e16f4a841b81663cdf698e394
HojatS <49037510+HojatS@users.noreply.github.com>
2023-02-21 15:58:53 -0800

    Update search.py

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ import copy
 import numpy as np
 import heapq
 
+
 # ENUMS
 UP = 0
 LEFT = 1
@@ -10,7 +11,7 @@ RIGHT = 3
 
 goal = [[1,2,3],[4,5,6],[7,8,0]]
 goal_flat = [1,2,3,4,5,6,7,8,0]
-    
+
 class Puzzle:
     def __init__(self, puzzle): 
         self.puzzle = puzzle
@@ -38,7 +39,7 @@ class Puzzle:
             neighbors.append([i_zero,j_zero-1, 3])
         
         puzzles = []
-        #swap neighbors with 0 to produce new child states
+        #swap neighbors with 0 and produce new child states
         for neighbor in neighbors:
             child_state = Puzzle(self.swap(neighbor[0], neighbor[1]))
             child_state.enum = neighbor[2]
@@ -76,8 +77,7 @@ class Puzzle:
     def TotalDistance(self):
         return self.gdistance + self.hdistance
     
-    
-    #comparison methods for heapq (with tie breaker)
+    #comparison methods for heapq
     def __gt__(self, other):
         if (self.TotalDistance() == other.TotalDistance()):
             return self.enum > other.enum
@@ -143,11 +143,14 @@ def BFS(board):
             visited.append(state)
             for child in state.child_states():
                 queue.append(child)
-
-    for node in visited:
-        final_solution.append(node.enum)
     
-    del final_solution[0]
+    path_node = state
+    while(path_node.parent != None):
+        final_solution.append(path_node.enum)
+        path_node = path_node.parent
+    
+    print("BFS: Number of visited Nodes: ", len(visited))
+    final_solution.reverse()
     return final_solution
 
 
@@ -173,10 +176,14 @@ def DFS(board):
                 stack.append(children[No_of_children - i - 1])
         
 
-    for node in visited:
-        final_solution.append(node.enum)
-
-    del final_solution[0]
+    path_node = state
+    while(path_node.parent != None):
+        final_solution.append(path_node.enum)
+        path_node = path_node.parent
+    
+    
+    print("DFS: Number of visited Nodes: ", len(visited))
+    final_solution.reverse()
     return final_solution
 
 
@@ -200,10 +207,13 @@ def A_Star_H1(board):
             for child in state.child_states():
                 heapq.heappush(queue, child)
 
-    for node in visited:
-        final_solution.append(node.enum)
-
-    del final_solution[0]
+    path_node = state
+    while(path_node.parent != None):
+        final_solution.append(path_node.enum)
+        path_node = path_node.parent
+    
+    print("A_Star_H1: Number of visited Nodes: ", len(visited))
+    final_solution.reverse()
     return final_solution
 
 
@@ -227,11 +237,16 @@ def A_Star_H2(board):
             for child in state.child_states():
                 heapq.heappush(queue, child)
 
-    for node in visited:
-        final_solution.append(node.enum)
-
-    del final_solution[0]
+    path_node = state
+    while(path_node.parent != None):
+        final_solution.append(path_node.enum)
+        path_node = path_node.parent
+    
+    print("A_Star_H2: Number of visited Nodes: ", len(visited))
+    final_solution.reverse()
     return final_solution
+
+
 
 
 
